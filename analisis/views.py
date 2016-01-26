a4349ffd5e623cd6fc649da966960e66ab4f816b
Ivan <ilgaleanos@gmail.com>
2016-01-26 10:24:33 -0500

    agregado esqueletos de graficas

--- analisis/views.py
+++ analisis/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+# -*- encoding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.cache import cache
+from django.db import transaction
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils import timezone
+from django.views.decorators.cache import cache_control
+from colaboradores.models import Colaboradores, ColaboradoresDatos
+from cuestionarios.models import  Preguntas, Variables, Respuestas
+from mensajeria.models import Streaming
+from usuarios.models import Proyectos, Logs
 
-# Create your views here.
+
+@cache_control(no_store=True)
+@login_required(login_url='/acceder/')
+def focalizados(request):
+	proyecto = cache.get(request.user.username)
+	if not proyecto:
+		return render_to_response('423.html')
+	permisos = request.user.permisos
+	if permisos.res_see:
+		# Ivan hace el superquery para mandar al js
+		return render_to_response('focalizado.html',{
+		'Activar':'AnalisisResultados','activar':'Focalizados',
+		'Proyecto':proyecto,'Permisos':permisos,
+		}, context_instance=RequestContext(request))
+	else:
+		return render_to_response('403.html')
+
+
+@cache_control(no_store=True)
+@login_required(login_url='/acceder/')
+def wordanalytics(request):
+	proyecto = cache.get(request.user.username)
+	if not proyecto:
+		return render_to_response('423.html')
+	permisos = request.user.permisos
+	if permisos.res_see:
+		# Lucho se encarga de hacer lo que necesita aqui
+		return render_to_response('wordanalytics.html',{
+		'Activar':'AnalisisResultados','activar':'WordAnalytics',
+		'Proyecto':proyecto,'Permisos':permisos,
+		}, context_instance=RequestContext(request))
+	else:
+		return render_to_response('403.html')
