5ea319de295537be9cd05f01742a5cb6bbf6f9af
andrade <nirvajara@hotmail.com>
2016-07-26 22:48:49 -0500

    supervisor

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as
 USER=root
-GROUP=gogo
+# GROUP=gogo
 cd /home/webapps/gogo/gogo
 . ../gogo_venv/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec ../gogo_venv/bin/gunicorn_django --timeout 400 -w $NUM_WORKERS \
---user=$USER --group=$GROUP --log-level=debug \
+--user=$USER --log-level=debug \
 --log-file=$LOGFILE 2>>$LOGFILE -b 127.0.0.1:8002 .
 
