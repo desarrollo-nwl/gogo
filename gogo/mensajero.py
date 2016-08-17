d8c534222f923fa3f930564c46f70d6269edd573
andrade <nirvajara@hotmail.com>
2016-08-17 08:02:13 -0500

    change cron

--- gogo/mensajero.py
+++ gogo/mensajero.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# import os,django,time
-# import sys
-# sys.path.append('/home/webapps/gogo/')
-# sys.path.append('/home/webapps/gogo/gogo/')
-# # sys.path.append('/home/suidi/Documentos/gogo/')
-# # sys.path.append('/home/suidi/Documentos/gogo/gogo/')
-# os.environ["DJANGO_SETTINGS_MODULE"] = "gogo.settings"
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# SECRET_KEY = '+xtgn6s8(15e#nv)1v5ta7n)*fpt=xq7+gt5o_28$8lzg3=ccm'
-# django.setup()
+import os,django,time
+import sys
+sys.path.append('/home/webapps/gogo/')
+sys.path.append('/home/webapps/gogo/gogo/')
+# sys.path.append('/home/suidi/Documentos/gogo/')
+# sys.path.append('/home/suidi/Documentos/gogo/gogo/')
+os.environ["DJANGO_SETTINGS_MODULE"] = "gogo.settings"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = '+xtgn6s8(15e#nv)1v5ta7n)*fpt=xq7+gt5o_28$8lzg3=ccm'
+django.setup()
 
 from colaboradores.models import *
 from mensajeria.corrector import salvar_html
